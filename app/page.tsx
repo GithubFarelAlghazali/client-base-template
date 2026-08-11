a17b514f78551eb56d41125f6e0724c6673017d1
GithubFarelAlghazali <farelgithub@gmail.com>
2026-08-11 13:31:32 +0700

    feat: change invalid ID page UI

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,11 @@ import { LiaUserLockSolid } from "react-icons/lia";
 
 export default async function Page() {
   return (
-    <main className="w-full h-screen flex flex-col gap-5 justify-center items-center">
-      <LiaUserLockSolid className="text-taupe-800 size-20" />
-      <h1 className="p-5 rounded-xl text-center bg-taupe-900 text-white shadow-xl">
-        ID tamu tidak valid
-      </h1>
+    <main className="w-full h-screen flex justify-center items-center">
+      <div className="rounded-xl shadow-2xl p-5 flex justify-center flex-col items-center outline-1 outline-taupe-900/50">
+        <LiaUserLockSolid className="text-taupe-800 size-20" />
+        <h1 className="p-5 text-center font-semibold">ID tamu tidak valid</h1>
+      </div>
     </main>
   );
 }
